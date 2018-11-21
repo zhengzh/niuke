d2adf57fae2b21dab16a864bc027b62bcdca8237
zhengzh <kszheng@mail.ustc.edu.cn>
2018-11-21 21:43:02 +0800

    dp

---
+++ niu_niu_de_bei_bao.py
@@ -0,0 +1,16 @@
+n, w = list(map(int, input().split()))
+v = list(map(int, input().split()))
+
+from functools import lru_cache
+
+@lru_cache(maxsize=32)
+def dfs(i, w):
+    print("(%d,%d)" % (i,w))
+    if w<0: return 0
+    if i==1:
+        if v[0]<=w:
+            return 2
+        return 1
+    return dfs(i-1, w) + dfs(i-1, w-v[i-1])
+
+print(dfs(n,w))
